544a307a2543d00920c63d8a4c1521dee82c3c61
vasu kamani <vasukamani.ce@gmail.com>
2024-11-10 15:31:17 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
   useEffect(() => {
     if(user){
-      const socketio = io('http://localhost:5000' , {
+      const socketio = io('https://instagram-vubs.onrender.com' , {
         query:{
           userId : user?._id
         },
